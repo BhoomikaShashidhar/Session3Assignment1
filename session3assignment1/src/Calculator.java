3684097e44fb47cfa0957bef24c8a402037dc321
BhoomikaShashidhar <bhoomi.dt87@gmail.com>
2016-07-13 23:42:18 +0530

    session3asssignment1
    Calculator

---
+++ session3assignment1/src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+	public static void main(String args[])
+	{
+double i=25d,j=27d;
+		double sqrt=Math.sqrt(i);
+		double cbrt=Math.cbrt(j);
+		System.out.println(" sqrt of "+ i +"=" +sqrt);
+		System.out.println(" cbrt of "+ j +"=" +cbrt);
+	}
+}
